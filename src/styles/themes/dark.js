0abc73b56ed7118d858d43c6d371316eafd6569e
FelipeLopes1996 <felipel.lima1996@gmail.com>
2022-02-18 15:02:33 -0300

    add dark theme

---
+++ src/styles/themes/dark.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  title: "dark",
+  bg: {
+    darkBlue: "#2b3945",
+    veryDarkBlue: "#fff",
+    veryLightGray: "#2b3945",
+    white: "#202c37",
+  },
+};
