d23828af500ec38290c5050f42d8fc7887e0968d
FelipeLopes1996 <felipel.lima1996@gmail.com>
2022-02-18 15:02:06 -0300

    add global style

---
+++ src/styles/global.js
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  *, *::before, *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Nunito Sans';
+    font-size: 14px;
+    
+  }
+
+a, button {
+  &, &:focus, &:active { outline: 'none'}
+}
+a {
+  text-decoration: none;
+}
+
+body {
+  font-size: 1rem;
+  font-family: 'sans-serif';
+  background-color: ${({ theme }) => theme.bg.white};
+    transition: ease-in-out 400ms;
+}
+`;
