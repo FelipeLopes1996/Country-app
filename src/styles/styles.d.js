0c39cde9637e32610c97de5c73199b657c1ae563
FelipeLopes1996 <felipel.lima1996@gmail.com>
2022-02-18 15:02:22 -0300

    add model style

---
+++ src/styles/styles.d.js
@@ -0,0 +1,31 @@
+// import "styled-components";
+
+// declare module 'styled-components' {
+export const defaultTheme = {
+  title: "",
+  bg: {
+    primary: "",
+    secondary: "",
+    success: "",
+    danger: "",
+    warning: "",
+    info: "",
+    body: "",
+    default: "",
+    light: "",
+    dark: "",
+  },
+  fg: {
+    primary: "",
+    secondary: "",
+    success: "",
+    danger: "",
+    warning: "",
+    info: "",
+    body: "",
+    default: "",
+    light: "",
+    dark: "",
+  },
+};
+// }
